c5a8202a8cf63b3a9420fd2f83bf161270eeef5e
DarthManu <manowski.b@gmail.com>
2020-04-13 17:57:42 +0200

    mf-01 add eslint airbnb

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,34 @@
 import React from 'react';
+import styled from 'styled-components';
 import logo from './logo.svg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const Test = styled.div`
+  background: pink;
+`;
+
+const App = () => (
+  <Test>
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit
+        {' '}
+        <code>
+          src/App.tsx
+        </code>
+        {' '}
+        and save to reload.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </header>
+  </Test>
+);
 
 export default App;

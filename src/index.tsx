2d381a3d25d659ec37814cdf8f1e1e1e5889d2e7
DarthManu <manowski.b@gmail.com>
2020-04-13 18:24:52 +0200

    mf-01 add normalize css

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { GlobalStyle } from './styles';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>,
   document.getElementById('root'),

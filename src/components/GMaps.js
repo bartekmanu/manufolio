644a9e9cd8b6b9c197a9cbbe0178e224d0b56999
DarthManu <manowski.b@gmail.com>
2020-04-18 11:36:14 +0200

    mf-09 add contact map in js file

---
+++ src/components/GMaps.js
@@ -0,0 +1,30 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/prefer-stateless-function */
+import React, { Component } from 'react';
+import { Map, GoogleApiWrapper } from 'google-maps-react';
+
+const mapStyles = {
+  width: '100%',
+  height: '100%',
+};
+
+export class MapContainer extends Component {
+  render() {
+    return (
+      <Map
+        google={this.props.google}
+        zoom={14}
+        style={mapStyles}
+        initialCenter={{
+          lat: -1.2884,
+          lng: 36.8233,
+        }}
+      />
+    );
+  }
+}
+
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyCmXRHlCydyksPOhmWTUdJLg0SXXA06l4I',
+})(MapContainer);

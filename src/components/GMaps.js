f8c298ff3fa21a0cfb854077c8d142b735564d73
DarthManu <manowski.b@gmail.com>
2020-04-18 13:38:27 +0200

    mf-09 working map component

--- src/components/GMaps.js
+++ src/components/GMaps.js
@@ -1,30 +1,113 @@
-/* eslint-disable react/prop-types */
-/* eslint-disable react/destructuring-assignment */
-/* eslint-disable react/prefer-stateless-function */
-import React, { Component } from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import React from 'react';
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
 
-const mapStyles = {
-  width: '100%',
-  height: '100%',
-};
+const googleMapURL =
+  'https://maps.googleapis.com/maps/api/js?key=AIzaSyCmXRHlCydyksPOhmWTUdJLg0SXXA06l4I&callback=initMap';
 
-export class MapContainer extends Component {
-  render() {
-    return (
-      <Map
-        google={this.props.google}
-        zoom={14}
-        style={mapStyles}
-        initialCenter={{
-          lat: -1.2884,
-          lng: 36.8233,
-        }}
-      />
-    );
-  }
-}
+const exampleMapStyles = [
+  { elementType: 'geometry', stylers: [{ color: '#242f3e' }] },
+  { elementType: 'labels.text.stroke', stylers: [{ color: '#242f3e' }] },
+  { elementType: 'labels.text.fill', stylers: [{ color: '#746855' }] },
+  {
+    featureType: 'administrative.locality',
+    elementType: 'labels.text.fill',
+    stylers: [{ color: '#d59563' }],
+  },
+  {
+    featureType: 'poi',
+    elementType: 'labels.text.fill',
+    stylers: [{ color: '#d59563' }],
+  },
+  {
+    featureType: 'poi.park',
+    elementType: 'geometry',
+    stylers: [{ color: '#263c3f' }],
+  },
+  {
+    featureType: 'poi.park',
+    elementType: 'labels.text.fill',
+    stylers: [{ color: '#6b9a76' }],
+  },
+  {
+    featureType: 'road',
+    elementType: 'geometry',
+    stylers: [{ color: '#38414e' }],
+  },
+  {
+    featureType: 'road',
+    elementType: 'geometry.stroke',
+    stylers: [{ color: '#212a37' }],
+  },
+  {
+    featureType: 'road',
+    elementType: 'labels.text.fill',
+    stylers: [{ color: '#9ca5b3' }],
+  },
+  {
+    featureType: 'road.highway',
+    elementType: 'geometry',
+    stylers: [{ color: '#746855' }],
+  },
+  {
+    featureType: 'road.highway',
+    elementType: 'geometry.stroke',
+    stylers: [{ color: '#1f2835' }],
+  },
+  {
+    featureType: 'road.highway',
+    elementType: 'labels.text.fill',
+    stylers: [{ color: '#f3d19c' }],
+  },
+  {
+    featureType: 'transit',
+    elementType: 'geometry',
+    stylers: [{ color: '#2f3948' }],
+  },
+  {
+    featureType: 'transit.station',
+    elementType: 'labels.text.fill',
+    stylers: [{ color: '#d59563' }],
+  },
+  {
+    featureType: 'water',
+    elementType: 'geometry',
+    stylers: [{ color: '#17263c' }],
+  },
+  {
+    featureType: 'water',
+    elementType: 'labels.text.fill',
+    stylers: [{ color: '#515c6d' }],
+  },
+  {
+    featureType: 'water',
+    elementType: 'labels.text.stroke',
+    stylers: [{ color: '#17263c' }],
+  },
+];
 
-export default GoogleApiWrapper({
-  apiKey: 'AIzaSyCmXRHlCydyksPOhmWTUdJLg0SXXA06l4I',
-})(MapContainer);
+const MyMapComponent = withScriptjs(
+  withGoogleMap((props) => (
+    <GoogleMap
+      defaultZoom={10}
+      defaultCenter={{ lat: 50.263497, lng: 19.024357 }}
+      defaultOptions={{ styles: exampleMapStyles }}
+      options={{ styles: exampleMapStyles }}
+    >
+      {props.isMarkerShown && <Marker position={{ lat: 50.263497, lng: 19.024357 }} />}
+    </GoogleMap>
+  )),
+);
+// 50.263497, 19.024357 Katowice
+// 51.107230, 17.036494 Wrocław
+// eslint-disable-next-line
+const Dupa = () => (
+  <MyMapComponent
+    isMarkerShown
+    googleMapURL={googleMapURL}
+    loadingElement={<div style={{ height: `100%` }} />}
+    containerElement={<div style={{ height: `400px` }} />}
+    mapElement={<div style={{ height: `100%` }} />}
+  />
+);
+
+export default Dupa;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import PageContentWrapper from '../components/PageContentWrapper';
 import PageHomeTitle from '../components/typography/PageHomeTitle';
 import portfolioLogo from '../assets/images/logo-circle.png';
-import GMaps from '../components/GMaps';
+import Dupa from '../components/GMaps';
 
 const Home: React.FC = () => (
   <PageContentWrapper
     pageTitle={<PageHomeTitle />}
     pageImg={portfolioLogo}
     pageImgAlt="portfolio-logo"
-    pageBody={<GMaps />}
+    pageBody={<Dupa />}
   />
 );
 
